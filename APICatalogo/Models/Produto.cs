2b5f199e487a4d2e65e3c831759a487b42977545
Vitor Mattos <vitorteixeira9@hotmail.com>
2024-05-14 17:53:47 -0300

    Adicionado a CategoriasController e realizado ajustes de performance.

--- APICatalogo/Models/Produto.cs
+++ APICatalogo/Models/Produto.cs
@@ -1,5 +1,6 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
 
 namespace APICatalogo.Models;
 public class Produto
@@ -21,5 +22,6 @@ public class Produto
     public float Estoque { get; set; }
     public DateTime DataCadastro { get; set;}
     public int CategoriaID { get; set; }
+    [JsonIgnore]
     public Categoria? Categoria { get; set; }
 }
